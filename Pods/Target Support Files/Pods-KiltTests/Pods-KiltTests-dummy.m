bea2ca03c96e39adaccb1b3813b470d29941b69d
Nurdaulet <b.nurdaulet.b@gmail.com>
2016-07-25 19:21:52 +0600

    Added pods into gitignore

--- Pods/Target Support Files/Pods-KiltTests/Pods-KiltTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KiltTests : NSObject
-@end
-@implementation PodsDummy_Pods_KiltTests
-@end
--- Pods/Target Support Files/Pods-KiltTests/Pods-KiltTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_KiltTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_KiltTestsVersionString[];
-
