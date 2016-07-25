a7d4c31c68be519eaa133ab6c17c3c71606fdc35
Nurdaulet <b.nurdaulet.b@gmail.com>
2016-07-25 10:21:43 +0600

    Initial Commit

---
+++ Kilt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kilt
+//
+//  Created by Nurdaulet Bolatov on 7/25/16.
+//  Copyright © 2016 Nurdaulet Bolatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
