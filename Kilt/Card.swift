c86d9cc5f8a50fb0a1cdef466ddb8737cfb761b1
Nurdaulet <b.nurdaulet.b@gmail.com>
2016-07-26 12:38:01 +0600

    Model of Card added

---
+++ Kilt/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  Kilt
+//
+//  Created by Nurdaulet Bolatov on 7/26/16.
+//  Copyright © 2016 Nurdaulet Bolatov. All rights reserved.
+//
+
+import UIKit
+
+struct Card {
+    let title: String?
+    let image: UIImage?
+}
--- Kilt/CardsViewModel.swift
+++ Kilt/CardsViewModel.swift
@@ -9,4 +9,5 @@
 import UIKit
 
 final class CardsViewModel {
+    internal var cards = [Card]()
 }
