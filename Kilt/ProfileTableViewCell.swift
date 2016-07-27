569e803894eaab52de2ece552c2f5684fd697b53
Nurdaulet <b.nurdaulet.b@gmail.com>
2016-07-27 15:28:51 +0600

    ProfileViewController & ProfileTableViewCell implemented

---
+++ Kilt/ProfileTableViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  ProfileTableViewCell.swift
+//  Kilt
+//
+//  Created by Nurdaulet Bolatov on 7/27/16.
+//  Copyright © 2016 Nurdaulet Bolatov. All rights reserved.
+//
+
+import UIKit
+import Sugar
+import Cartography
+
+final class ProfileTableViewCell: UITableViewCell {
+    
+    private lazy var titleLabel = UILabel().then {
+        $0.textAlignment = .Left
+        $0.textColor = .blackColor()
+        $0.font = .systemFontOfSize(15)
+    }
+    
+    private lazy var subtitleLabel = UILabel().then {
+        $0.textAlignment = .Right
+        $0.textColor = .mountainMistColor()
+        $0.font = .systemFontOfSize(15)
+        $0.setContentHuggingPriority(1000, forAxis: .Horizontal)
+    }
+    
+    private lazy var iconImageView = UIImageView().then {
+        $0.contentMode = .ScaleAspectFill
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpViews()
+        setUpConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpViews() {
+        selectionStyle = .None
+        [titleLabel, subtitleLabel, iconImageView].forEach { contentView.addSubview($0) }
+    }
+    
+    private func setUpConstraints() {
+        constrain(titleLabel, subtitleLabel, iconImageView, contentView) {
+            titleLabel, subtitleLabel, iconImageView, contentView in
+            iconImageView.leading == contentView.leading + 10
+            iconImageView.centerY == contentView.centerY
+            iconImageView.width == 32
+            iconImageView.height == 32
+            
+            subtitleLabel.trailing == contentView.trailing - 15
+            subtitleLabel.centerY == contentView.centerY
+            
+            titleLabel.leading == iconImageView.trailing + 15
+            titleLabel.centerY == contentView.centerY
+            titleLabel.trailing == subtitleLabel.leading - 10
+        }
+    }
+    
+}
+
+extension ProfileTableViewCell {
+    func setUpWithTitle(title: String?, subtitle: String?, icon: UIImage?) {
+        titleLabel.text = title
+        subtitleLabel.text = subtitle
+        iconImageView.image = icon
+    }
+}
--- Kilt/ProfileViewController.swift
+++ Kilt/ProfileViewController.swift
@@ -7,7 +7,73 @@
 //
 
 import UIKit
+import Sugar
+import Cartography
 
 final class ProfileViewController: UIViewController {
 
+    private let profileCellIdentifier = "profileCellIdentifier"
+    
+    private lazy var tableView: UITableView = {
+        return UITableView().then {
+            $0.delegate = self
+            $0.dataSource = self
+            $0.rowHeight = 44
+            $0.tableFooterView = UIView()
+            $0.registerClass(ProfileTableViewCell.self, forCellReuseIdentifier: self.profileCellIdentifier)
+        }
+    }()
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpViews()
+        setUpConstraints()
+    }
+    
+    // MARK: Set Up
+    
+    private func setUpViews() {
+        edgesForExtendedLayout = .None
+        view.backgroundColor = .whiteColor()
+        navigationItem.title = "Карточки"
+        view.addSubview(tableView)
+    }
+    
+    private func setUpConstraints() {
+        constrain(tableView, view) {
+            $0.edges == $1.edges
+        }
+    }
+    
+}
+
+// MARK: UITableViewDelegate
+
+extension ProfileViewController: UITableViewDelegate {
+}
+
+// MARK: UITableViewDataSource
+
+extension ProfileViewController: UITableViewDataSource {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: return 2
+        case 1: return 1
+        default: return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return (tableView.dequeueReusableCellWithIdentifier(profileCellIdentifier, forIndexPath: indexPath) as! ProfileTableViewCell).then {
+            $0.setUpWithTitle("asd", subtitle: "asd", icon: nil)
+        }
+    }
+    
 }
